ef2a0ce019f0c02da0b007030201fd7c5b1b1a31
Nicolas Poirier-Barolet <nicolas.poirierbarolet@gmail.com>
2013-08-10 21:52:30 -0400

    Started the demo page

---
+++ scripts/demo.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// DEFINE CUSTOM SETTINGS OR LET AN EMPTY
+	// OBJECT FOR DEFAULTS
+	var neutrinoSettings = {};
+
+	// CREATE A NEW NEUTRINO OBJECT
+	var neutrino = Neutrino();
+
+	// INITIALIZE NEUTRINO
+	neutrino.init(neutrinoSettings);
+});
--- scripts/neutrino.js
+++ scripts/neutrino.js
@@ -2,6 +2,37 @@
 // Closure so that variables don't get
 // involved with variables in your code
 //=====================================
-function() {
+function Neutrino() {
+	var neutrino = {
+		params : {},
 
-}();
+		init : function(settings) {
+			this.root = $('.neutrino');
+			this.slides = this.root.find('.slide');
+
+			this._setParams(settings);
+			this.currentIndex = 0;
+
+			this._initSlide();
+		},
+
+		_setParams : function(settings) {
+			this.params = {
+				width: settings.width || this.root.width(),
+				height: settings.height || this.root.height(),
+				transitionType: settings.transitionType || 'slide',
+				transitionTime: settings.transitionTime || '0.75',
+				slideWidth: settings.slideWidth || this.slides.eq(0).width(),
+				timer: settings.timer || '3500',
+				hasArrows: settings.hasArrows || false,
+				hasNav: settings.hasNav || true
+			}
+		},
+
+		_initSlide : function(){
+			
+		}
+	}
+
+	return neutrino;
+};
